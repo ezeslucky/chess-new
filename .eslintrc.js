abfe2e8fa9b25a71bf2ad54c1d9efd909259f507
Lucky <lucky123@gmail.com>
2024-11-13 18:54:31 +0530

    file add

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// .eslintrc.js
+module.exports = {
+    extends: ["@repo/eslint-config/library.js"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: true,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-redeclare": "off",
+       "turbo/no-undeclared-env-vars": "off",
+    }
+  };
+  
